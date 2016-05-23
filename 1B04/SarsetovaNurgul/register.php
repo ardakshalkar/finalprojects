135a20d039a55e566068d1da295c7933201efddc
NurgulSarsetova <nurgul9757@gmail.com>
2016-05-23 12:38:18 +0600

    Create register.php

---
+++ 1B04/SarsetovaNurgul/register.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</style>
+</head>
+<body>
+
+<body style="background color:lightblue">
+
+
+<?php
+include("header.php")
+?>
+
+<div id="nav">
+<div class="menuitem">
+<a href="project.php">Main</a><br></div>
+<div class="menuitem">
+<a href="4444.php">Diamonds</a><br></div>
+
+<div class= "menuitem">
+<a href="jewellery.php">Weddings</a><br></div>
+<div class="menuitem">
+<a href="watches.php">Watches</a><br></div>
+<div class="menuitem">
+<a href="sale.php">Sale</a>
+</div>
+</div>
+
+<form name="register" action="regi.php" method="GET"> 
+<div class="sign">
+
+Name:<br>
+<input type="text" name="name">
+<br>
+Surname:<br>
+<input type="text" name="surname">
+<br>
+Email:<br>
+<input type="text" name="email" >
+<br>
+Password:<br>
+<input type="password" name="password">
+<br>
+
+<input type="submit" class="submit" value="submit" />
+</div> 
+</form>
+ </form>    
+				  <div><?php if(isset($_GET['register'])) echo "<span style='color:red'>Registration Successful</span>";?></div> 
+                </div>  
+
+</div>
+
+<?php
+include("footer3.php");
+?>
+
+</body>
+</html>
+
+</style>
+</head>
