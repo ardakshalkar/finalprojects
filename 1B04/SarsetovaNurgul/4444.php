bccbf9248000ffdfaeb7b96c215ee2a38bb48942
NurgulSarsetova <nurgul9757@gmail.com>
2016-05-23 12:03:28 +0600

    Create 4444.php

---
+++ 1B04/SarsetovaNurgul/4444.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="style.css"/>
+
+</style>
+
+<?php
+include("header.php")
+?>
+</head>
+<body>
+<body style="background color:lightblue">
+
+<!-- <div id="header">
+<h1>GOLDSMITHS</h1>
+<p>since 1778</p>
+<hr>
+<div class="topleft">&#128222; 8747 817 37 97</div>
+<div class="topright"><a href="file:///C:/Users/%D0%9D%D1%83%D1%80%D0%B3%D1%83%D0%BB%D1%8C/Desktop/hw/prob2.html">Sign in or register</a>
+<p><input type="search" id="mySearch" placeholder="Search "></p></div>
+</div>
+ -->
+
+<div id="nav">
+<div class="menuitem">
+<a href="project.php">Main</a><br></div>
+<div class="menuitem">
+<a href="4444.php">Diamonds</a><br></div>
+
+<div class= "menuitem">
+<a href="jewellery.php">Weddings</a><br></div>
+<div class="menuitem">
+<a href="watches.php">Watches</a><br></div>
+<div class="menuitem">
+<a href="sale.php">Sale</a>
+</div>
+</div>
+<div id="section">
+<p><img src="http://www.goldsmiths.co.uk/medias/?context=bWFzdGVyfHJvb3R8MjM5MzYxfGltYWdlL2pwZWd8aDJkL2hkYi84OTE1ODg0Mjc3NzkwLmpwZ3xlZDU3YjBhMWQ4NDZlYmYzNmJiYjE5ZWJhYzMwMDRiNDEyMGMyMTI1Y2M2YTNlODcyZGUwZGY3M2ZiMDY5OTc5"></p></div>
+
+<div class="brand0"><a href="link33.php">
+<img src="http://www.goldsmiths.co.uk/medias/?context=bWFzdGVyfHJvb3R8MjcxODB8aW1hZ2UvanBlZ3xoMGQvaGFlLzg4OTU0NTg3NzA5NzQuanBnfDVkNWVjNTQ2MWI4OGMwNWU3ZmQ1NTA0MGFiNDk0MGFhNmNjNzk5ODk0M2I1ZDQ1MjgyMGU2MzQ5YzViNzBiMDQ" width="250"></a></div>
+<div class="brand11"><a href="link22.php">
+<img src="http://www.goldsmiths.co.uk/medias/?context=bWFzdGVyfHJvb3R8MjcxNTR8aW1hZ2UvanBlZ3xoYmEvaDlmLzg4OTU0NTM2NTkxNjYuanBnfDhmZjI0M2VkOWM5ZjYxYzRhNzhkNTFhYjBjOWMwMDVmMmU1MjE1NzJmNjk3MDdkNjU0NzNlOWVhNDczMzQ1ODQ" width="250"></a></div>
+<div class="brand22"><a href="link11.php">
+<img src="http://www.goldsmiths.co.uk/medias/?context=bWFzdGVyfHJvb3R8MzQ0NDh8aW1hZ2UvanBlZ3xoMDYvaDBhLzg4OTUzNDg2MDQ5NTguanBnfDM4ZjBkODBjMjUyODgwMDJlOTkwYzc4N2M2N2U0YjIxODE2YzA2NGQxNjBhZTQ4NTRjYTI5MjdhMWQwZDJiY2Y" width="250"></a></div>
+
+
+
+
+
+
+
+<?php
+include("footer1.php");
+?>
+
+
+</body>
+</html>
+ 
