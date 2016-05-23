95e0ef8fca3bb00beefd23f321a2bc25ccddb010
NurgulSarsetova <nurgul9757@gmail.com>
2016-05-23 12:38:45 +0600

    Create sale.php

---
+++ 1B04/SarsetovaNurgul/sale.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="style.css"/>
+
+
+</head>
+<body>
+<body style="background color:lightblue">
+<?php
+include("header.php");
+?>
+
+
+<div id="nav">
+<div class="menuitem">
+<a href="project.php">Main</a><br></div>
+<div class="menuitem">
+<a href="4444.php">Diamonds</a><br></div>
+
+<div class= "menuitem">
+<a href="jewellery.php">Weddings</a><br></div>
+<div class="menuitem">
+<a href="watches.php">Watches</a><br></div>
+<div class="menuitem">
+<a href="sale.php">Sale</a>
+</div>
+</div>
+<div id="section">
+<p><img src="http://www.goldsmiths.co.uk/medias/?context=bWFzdGVyfHJvb3R8Mjc5MzN8aW1hZ2UvanBlZ3xoNjQvaDNhLzg5MTY2NDI3NTg2ODYuanBnfDJlYWIzYjJmYzE2YjVmMWM2YTViYWE2OWViNWZiNjAzMzkzNDg2NjMwYzJjMDQwMGE0ZTcxYWY1M2UzMDRiN2Q"></p></div>
+
+<div class="brand0">
+<img src="http://www.goldsmiths.co.uk/medias/?context=bWFzdGVyfHJvb3R8MTQwMjV8aW1hZ2UvanBlZ3xoMWIvaGNjLzg5MTY2NDI4ODk3NTguanBnfDdiZDc3NjcxMWIzNzBlZDNmODQ5YzdmYTBlNGIzY2M2MzY1OGY2MmVjZjU4MmNhYjdjNWIzNDc5MTAzMmUwMDc" width="250"></div>
+<div class="brand11">
+<img src="http://www.goldsmiths.co.uk/medias/?context=bWFzdGVyfHJvb3R8MzMzMDV8aW1hZ2UvanBlZ3xoMDcvaDdkLzg5MTY2NDMyMTc0MzguanBnfDdhMzNkZWZlOTJkYjM4YjIwMTRhN2ZlYjc3MDIwODE1ZGFjODZlYjc0NmZiYjRiNmM3YjBjNTg5OTIzMWMwMGE" width="250"></div>
+<div class="brand22">
+<img src="http://www.goldsmiths.co.uk/medias/?context=bWFzdGVyfHJvb3R8MTgxNTF8aW1hZ2UvanBlZ3xoYzkvaDUxLzg5MTY2NDI4MjQyMjIuanBnfDAyNDY3YTI5MzJiOGY0ZjZhYWNkY2VjYjI0ODU0NDNkYzdjYTYzNmFmZGM4OTk0ZTUzMWRkNGRlNGFhNjYwYzM" width="250"></div>
+<div class="brand33">
+<img src="http://www.goldsmiths.co.uk/medias/?context=bWFzdGVyfHJvb3R8MTM2ODB8aW1hZ2UvanBlZ3xoZWMvaDliLzg5MTY2NDMxNTE5MDIuanBnfDlkNDc5MmU3ODMxNTRjOGY5ZGEyN2Y3ZDhkNjNlM2I4NDg5M2Y1NzViODJmNGI5OTA2YmE0ZjYxZWM0ZTIxMTI" width="250"></div>
+
+
+
+<?php
+include("footer2.php");
+?>
+
+</body>
+</html>
+ 
