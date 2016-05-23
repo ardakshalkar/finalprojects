70925d34a3da1374160eb62382d77b0b6c254011
NurgulSarsetova <nurgul9757@gmail.com>
2016-05-23 12:10:19 +0600

    Create footer1.php

---
+++ 1B04/SarsetovaNurgul/footer1.php
@@ -0,0 +1,14 @@
+<div class="footer1">
+
+<button class="button1">Find Us Here</button>
+
+<a href="https://vk.com/audios195818109" button class="button"><img src="http://www.bccapper.ru/images/Mezhdunarodny_logotip_VK.png" style="float:right;width:20px;">
+</button></a>
+<a href="https://instagram.com/goldsmiths_uk/" button class="button"><img src="http://static.wixstatic.com/media/5fc307_0d57813335a74253a8b0e10aef8bb443.png" style="float:right;width:20px;" ></button></a>
+<a href="https://twitter.com/Goldsmithsuk" button class="button"><img src="http://www.ccvf.pt/images/fb_logo.png" style="float:right;width:20px;" ></button></a>
+<a href="https://www.facebook.com/pages/Goldsmiths/369815513040048" button class="button"><img src="https://g.twimg.com/Twitter_logo_white.png" style="float:right;width:20px;" ></button></a>
+
+&#9819; <a href="about.php"><span style="cursor:help">About Us</span></a><br>
+     &#9819; <a href="#">Store Locator</a><br>
+    &#9819; <a href="contact.php">Contact Us</a>
+</div>
