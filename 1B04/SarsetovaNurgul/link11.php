2d02b07cfcdc88619dcd0d655d9e269c41480851
NurgulSarsetova <nurgul9757@gmail.com>
2016-05-23 12:23:15 +0600

    Create link11.php

---
+++ 1B04/SarsetovaNurgul/link11.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="style.css"
+</head>
+<body>
+<body style="background color:lightblue">
+<?php
+include("header.php");
+?>
+
+<div id="nav">
+<div class="menuitem">
+<a href="project.php">Main</a><br></div>
+<div class="menuitem">
+<a href="4444.php">Diamonds</a><br></div>
+
+<div class= "menuitem">
+<a href="jewellery.php">Weddings</a><br></div>
+<div class="menuitem">
+<a href="watches.php">Watches</a><br></div>
+<div class="menuitem">
+<a href="sale.php">Sale</a>
+</div>
+</div>
+<div class="cart"> 
+    <img src="http://static.wixstatic.com/media/372f84_4d94fb014c4cc4cc0ea64bd7d2e38c48.jpg_1024" width=200px; height="200px" /> 
+  <a href="#" class="price">
+    &#163; 2,000,00 
+  </a> 
+  
+</div> 
+<div class="cart"> 
+    <img src="http://l1.sl-love.ru/236/upload/iblock/f63/8858.jpg?1422520580" width=200px; height="200px" />
+  <a href="#" class="price"> 
+    &#163; 1,900,00
+  </a> 
+  
+</div> 
+<div class="cart"> 
+  <img src="http://zolotoigid.ru/wp-content/uploads/2015/04/sergi_iz_zolota_03.jpg" width=200px; height="200px" /> 
+   <a href="#" class="price"> 
+   &#163; 2,600,00 
+   </a> 
+    
+</div> 
+
+<div class="cart"> 
+<img src="http://karatov.ru/upload/iblock/6ec/6ecfd2b38dc74b0dd0db742523c598f6.jpg" width=200px; height="200px" /> 
+<a href="#" class="price"> 
+&#163; 2,300,00 
+</a> 
+ 
+</div> 
+<div class="cart"> 
+<img src="http://www.goldgoldgold.ru/img/tovars/big/1519.jpg" width=200px; height="200px" /> 
+<a href="#" class="price"> 
+&#163; 2,800,00
+</a> 
+
+</div> 
+<div class="cart"> 
+<img src="http://www.clarajewellery.com/wp-content/uploads//cewdetails.jpg" width=200px; height="200px" /> 
+<a href="#" class="price"> 
+&#163; 2,000,00
+</a> 
+
+</div>
+
+
+<div class="cart"> 
+<img src="http://www.bribri.ru/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/5/1/51592.jpg" width=200px; height="200px" /> 
+<a href="#" class="price"> 
+&#163; 1,000,00
+</a> 
+ 
+</div> 
+<div class="cart"> 
+<img src="http://www.gold4u.ru/pictures/large/VILARS4M.jpg" width=200px; height="200px" /> 
+<a href="#" class="price"> 
+&#163; 600,00
+</a> 
+
+</div>
+
+
+
+<div class="cart"> 
+<img src="http://www.goldium.ru/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/2/E/2ERP7S622677.jpg" width=200px; height="200px" /> 
+<a href="#" class="price"> 
+&#163; 1,500,00 
+</a> 
+
+</div> 
+<div class="cart"> 
+<img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQd17ZHrTXlC0tIjnWRw19U5rvP46f6rdqZZjPdZC-NewwaGHjB" width=200px; height="200px" /> 
+<a href="#" class="price"> 
+&#163; 2,000,00
+</a> 
+
+</div>
+
+<?php
+include("footer2.php");
+?>
+
+
+
+</body>
+</html>
+
+</style>
+</head>
