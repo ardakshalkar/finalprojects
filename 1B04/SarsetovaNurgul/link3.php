5a04bb90e92998130304613da1147c48a895f603
NurgulSarsetova <nurgul9757@gmail.com>
2016-05-23 12:32:12 +0600

    Create link3.php

---
+++ 1B04/SarsetovaNurgul/link3.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="style.css"
+</head>
+<body>
+<body style="background color:lightblue">
+
+
+<?php
+include("header.php");
+?>
+
+<div id="nav">
+<div class="menuitem">
+<a href="project.php">Main</a><br></div>
+<div class="menuitem">
+<a href="4444.php">Diamonds</a><br></div>
+
+<div class= "menuitem">
+<a href="jewellery.php">Weddings</a><br></div>
+<div class="menuitem">
+<a href="watches.php">Watches</a><br></div>
+<div class="menuitem">
+<a href="sale.php">Sale</a>
+</div>
+</div>
+<div class="cart"> 
+    <img src="http://lutin.kz/wp-content/uploads/2012/07/M4.jpg" width=200px;/> 
+  <a href="#" class="price">
+    &#163; 2,000,00 
+  </a> 
+  
+</div> 
+<div class="cart"> 
+    <img src="http://obruchalnye-kolca.juveros-shop.ru/upload/iblock/5a6/5a67dbe608746f5362d415f2db3d73c0.jpg" width=200px;/>
+  <a href="#" class="price"> 
+    &#163; 1,900,00
+  </a> 
+  
+</div> 
+<div class="cart"> 
+  <img src="http://cs309321.vk.me/v309321097/1ea0/QDRgHCNgvGk.jpg" width=200px;/> 
+   <a href="#" class="price"> 
+   &#163; 2,600,00 
+   </a> 
+    
+</div> 
+
+<div class="cart"> 
+<img src="http://weddingdate.ru/wp-content/gallery/muzhskoe-s-brilliantom/3_1394_14062012161009_1st.jpg" width=200px;/> 
+<a href="#" class="price"> 
+&#163; 2,300,00 
+</a> 
+ 
+</div> 
+<div class="cart"> 
+<img src="http://obruchalki.com/upload/resize_cache/iblock/56e/1000_1000_1c77789a99dbc1175cb6c669c44b7c098/hm0091_rsi.jpg" width=200px;/> 
+<a href="#" class="price"> 
+&#163; 2,800,00
+</a> 
+
+</div> 
+<div class="cart"> 
+<img src="http://i.rings.ru/45a4d3bac91726cc8d7ac5bce4d5621d.jpg" width=200px;/> 
+<a href="#" class="price"> 
+&#163; 2,000,00
+</a> 
+
+</div>
+
+
+<div class="cart"> 
+<img src="http://grandstock.ru/media/blog/muzhskie_obruchalnye_kolca.jpg" width=200px;/> 
+<a href="#" class="price"> 
+&#163; 1,000,00
+</a> 
+ 
+</div> 
+<div class="cart"> 
+<img src="http://golden-birds.ru/wp-content/uploads/2014/05/6.jpg" width=200px;/> 
+<a href="#" class="price"> 
+&#163; 600,00
+</a> 
+
+</div>
+
+
+
+<div class="cart"> 
+<img src="http://g01.a.alicdn.com/kf/HTB1.vasIFXXXXbBXpXXq6xXFXXXv/-font-b-Ti-b-font-accessories-fashion-male-wide-font-b-rings-b-font-wire.jpg" width=200px;/> 
+<a href="#" class="price"> 
+&#163; 1,500,00 
+</a> 
+
+</div> 
+<div class="cart"> 
+<img src="http://bride-style.ru/blog/uploads/images/00/00/03/2011/02/26/d4184f.jpg" width=200px;/> 
+<a href="#" class="price"> 
+&#163; 2,000,00
+</a> 
+
+</div>
+
+
+
+<?php
+include("footer2.php");
+?>
+
+</body>
+</html>
+
+</style>
+</head>
